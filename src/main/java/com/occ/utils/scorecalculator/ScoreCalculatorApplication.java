5545b6b44447b534c7f55e035641ca8c98eb5e13
Praveen Gajjala <praveengajjala99@gmail.com>
2020-04-11 12:40:33 -0500

    initial commit; setup and scaffolding

---
+++ src/main/java/com/occ/utils/scorecalculator/ScoreCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.occ.utils.scorecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/occ/utils/scorecalculator/ScoreCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.occ.utils.scorecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
