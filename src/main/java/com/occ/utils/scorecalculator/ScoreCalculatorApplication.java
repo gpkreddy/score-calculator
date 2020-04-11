a73aa987ba262511a528442e6377b63c96af4f3d
Praveen Gajjala <praveengajjala99@gmail.com>
2020-04-11 13:02:30 -0500

    add logger and service; make command line runner

--- src/main/java/com/occ/utils/scorecalculator/ScoreCalculatorApplication.java
+++ src/main/java/com/occ/utils/scorecalculator/ScoreCalculatorApplication.java
@@ -1,13 +1,31 @@
 package com.occ.utils.scorecalculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScoreCalculatorApplication {
+public class ScoreCalculatorApplication implements CommandLineRunner{
 
-	public static void main(String[] args) {
+	private static Logger LOG = LoggerFactory.getLogger(ScoreCalculatorApplication.class);
+
+	@Autowired
+	private ScoreCalculatorService scoreCalculatorService;
+
+	public static void main(final String[] args) {
+		LOG.info("Starting the Score Calculator program");
 		SpringApplication.run(ScoreCalculatorApplication.class, args);
+		LOG.info("End of Score Calculator Program");
+	}
+
+	@Override
+	public void run(final String... args) throws Exception {
+		
+		LOG.info("in run method");
+
 	}
 
 }
---
+++ src/main/java/com/occ/utils/scorecalculator/ScoreCalculatorService.java
@@ -0,0 +1,19 @@
+
+/*
+ * Copyright (c) ABC Systems. All rights reserved.  
+ * Licensed under ABC Systems. See LICENSE file in the project root for full license information.  
+ *   
+ */
+
+package com.occ.utils.scorecalculator;
+
+import org.springframework.stereotype.Service;
+
+/* created by Praveen Gajjala
+ *  created on 4/11/2020
+ */
+
+ @Service
+public class ScoreCalculatorService {
+
+}
